1e7b7ac73fddb357b764c0225a6ca27ed2fede43
nlpteam <hieunguyenquoc1999@gmail.com>
2022-05-17 22:39:40 +0700

    Upload code

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var textInp = document.querySelector("textarea[name='txtinput']");
 const btn = document.querySelector("#btn");
 var loader = $("#loader")
-// var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-// var serverUrl = "https://api.funtranslations.com/translate/minion.json";
-// var serverUrl = "http://localhost:8000/Translate_Vi_La";
+var fileNameUrl = ''
+    // var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+    // var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+    // var serverUrl = "http://localhost:8000/Translate_Vi_La";
 async function tranlate() {
     var data = $('#arrowLR').attr('data-current');
     if (data === 'VN-L') {
@@ -44,40 +45,53 @@ async function translateLaoViet() {
     console.log("Hello: ", result);
     $('#box2 .card-body').html(result)
 }
-async function translateViLaoDoc() {
-    var serverUrl = `http://127.0.0.1:8000/Translate_Vi_Lao_Docs/`;
 
+async function uploadAndTranslateFile() {
+    var data = $('#arrowLR2').attr('data-current');
+    var result = '';
+    if (data === 'VN-L') {
+        var serverUrl = `http://127.0.0.1:8000/Translate_Vi_Lao_Docs/`;
+        result = await translateFile(serverUrl);
+    } else {
+        var serverUrl = `http://127.0.0.1:8000/Translate_Lao_Vi_Docs/`;
+        result = await translateFile(serverUrl);
+    }
+    // return rs;
+    var blob = new Blob([result], { type: "text/plain;charset=utf-8" });
+    var d = new Date();
+    var extendFile = "result_" + data + "_" + d.getFullYear() + d.getMonth() + d.getDate() + d.getHours() + d.getMinutes() + d.getSeconds();
+    console.log("extend:", extendFile);
+    await saveAs(blob, `${extendFile}.txt`);
+    $("#formFileMultiple").replaceWith($("#formFileMultiple").val('').clone(true));
+    // $('#formFileMultiple').val = ''
+    // alert('You have successfully upload the file!');
 }
-
-async function uploadFile() {
-    var serverUrl = `http://127.0.0.1:8000/Translate_Vi_Lao_Docs/`;
+async function translateFile(serverUrl) {
+    // var serverUrl = `http://127.0.0.1:8000/Translate_Vi_Lao_Docs/`;
+    var rs = ' ';
     //creating form data object and append file into that form data
     let formData = new FormData();
-    formData.append("file", formFileMultiple.files[0]);
-
-    console.log("test", formData);
-    //network request using POST method of fetch
-    var rs = await fetch(serverUrl, {
-        method: "POST",
-        body: formData
-    })
-    .then(() => {
+    fileNameUrl = formFileMultiple.files[0]
+    if (fileNameUrl) {
         loader.addClass('loader__element')
-    })
-    .then(res => {
-        setTimeout(() => {
-            res.json()
-        loader.addClass('')
-        }, 3000)
-    });
-    console.log(`rs: `, rs);
-    // return rs;
-    var blob = new Blob([rs], { type: "text/plain;charset=utf-8" });
-    var d = new Date();
-    var extendFile = "result_" + d.getHours() + "_" + d.getMinutes() + "_" + d.getSeconds();
-    console.log("extend:", extendFile);
-    saveAs(blob, `${extendFile}.txt`);
-    // alert('You have successfully upload the file!');
+        formData.append("file", fileNameUrl);
+
+        console.log("test", formData);
+        //network request using POST method of fetch
+        rs = await fetch(serverUrl, {
+                method: "POST",
+                body: formData
+            })
+            .then(res => {
+                console.log("res: ", res)
+                return res.json()
+            })
+
+        loader.removeClass('loader__element')
+
+        console.log(`rs: `, rs);
+    }
+    return rs;
 }
 
 $('#arrowLR').on('click', function() {
@@ -120,4 +134,10 @@ $('#pills-profile-tab').on('click', function() {
 })
 $('#pills-home-tab').on('click', function() {
     $('#pills-home').css('display', 'block')
+})
+
+$('.btn-wapper').on('click', function() {
+    const dropArea = document.querySelector(".drop_box")
+    const input = dropArea.querySelector("input");
+    input.click()
 })
